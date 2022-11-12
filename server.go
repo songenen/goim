39892efe3bc4435f4d44300df76eaa8b6df92786
songen <614750803@qq.com>
2022-11-12 23:58:40 +0800

    广播user上线

--- server.go
+++ server.go
@@ -3,24 +3,71 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播
+	Message chan string
 }
 
 // 创建一个Server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 
 }
+
+// 监听Message广播消息channel的goroutine，一旦有消息发送给全部在线的User
+func (this *Server) ListenMessager() {
+	for {
+
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
+	//链接业务
 	fmt.Println("建立成功。。。")
+
+	user := NewUser(conn)
+
+	//用户上线加入onlineMap
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.BroadCast(user, "online")
+
+	select {}
+
 }
 
 // 启动接口服务
@@ -33,6 +80,9 @@ func (this *Server) Start() {
 	}
 	// close listen
 	defer listener.Close()
+
+	//启动监听Message的goroutine
+	go this.ListenMessager()
 
 	for {
 		//accept
